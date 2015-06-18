c6538c0b82f26433345872a5ad1e2a8b696f7398
John Da Silva <johnds@hotmail.co.uk>
2015-06-18 17:47:55 +0100

    First commit. Still broken.

---
+++ Samples/SimpleTest/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdr.JsonMagic;
+
+namespace SimpleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //            JsConfig.With(maxDepth: 1);
+
+                EntityModel model = new EntityModel("TestModel");
+
+                IGxPoint pt1 = new GxPoint();
+                pt1.Name = "pt1";
+                pt1.X = 100;
+
+                IGxPoint pt2 = new GxPoint();
+                pt2.Name = "pt2";
+                pt2.X = 200;
+                pt2.AddPar(pt1);
+
+                model.AddEntity(pt1);
+                model.AddEntity(pt2);
+
+                string j;
+
+                j = JsonMagic.ToJson(new List<int> { 1, 2, 3, 4 });
+
+                j = JsonMagic.ToJson(model);
+
+                j = JsonMagic.ToJson("Hello");
+
+                j = JsonMagic.ToJson(new[] { 10, 20, 30 });
+
+/*
+                var json = JsonConvert.SerializeObject(model, Formatting.Indented, new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                    TypeNameHandling = TypeNameHandling.Auto,
+                    Converters = new[] { new MyConverter(), }
+                });
+
+                j = JsonConvert.SerializeObject("Hello");
+                j = JsonMagic.ToJson("Hello");
+
+                j = JsonConvert.SerializeObject(new[] { 10, 20, 30 });
+                j = JsonMagic.ToJson(new[] { 10, 20, 30 });
+
+                var mdl = JsonConvert.DeserializeObject<EntityModel>(json, new JsonSerializerSettings
+                {
+                    PreserveReferencesHandling = PreserveReferencesHandling.All,
+                    TypeNameHandling = TypeNameHandling.Auto,
+                    Converters = new[] { new MyConverter(), }
+                });
+*/
+
+                //            var json = JsonSerializer.SerializeToString(model);
+
+                //            var mdl = JsonSerializer.DeserializeFromString<EntityModel>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: {0}", ex);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+/*
+    class MyConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is IDictionary<IGxEntity, int>)
+            {
+                var list = ((IDictionary<IGxEntity, int>)value).ToList();
+
+                serializer.Serialize(writer, list);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+
+            JObject jobj = JObject.Load(reader);
+
+            return null;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            Console.WriteLine("CanConvert: {0}", objectType);
+
+            return typeof(IDictionary<IGxEntity, int>).IsAssignableFrom(objectType);
+        }
+    }
+*/
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EntityModel
+    {
+
+        public string Name { get; set; }
+        public IList<IGxEntity> Entities { get; set; }
+
+        public EntityModel(string name)
+        {
+            Name = name;
+        }
+
+        public void AddEntity(IGxEntity entity)
+        {
+            if (Entities == null)
+            {
+                Entities = new List<IGxEntity>();
+            }
+
+            Entities.Add(entity);
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IGxEntity
+    {
+        string Name { get; set; }
+        IDictionary<IGxEntity, int> Dependants { get; set; }
+        IDictionary<IGxEntity, int> Parents { get; set; }
+
+        void AddDep(IGxEntity dep);
+        void AddPar(IGxEntity dep);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IGxPoint : IGxEntity
+    {
+        int X { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class GxEntityBase : IGxEntity
+    {
+        private IDictionary<IGxEntity, int> _dependants;
+        private IDictionary<IGxEntity, int> _parents;
+
+        public GxEntityBase()
+        {
+            Dependants = new Dictionary<IGxEntity, int>();
+            Parents = new Dictionary<IGxEntity, int>();
+        }
+
+        public string Name { get; set; }
+
+        public IDictionary<IGxEntity, int> Dependants
+        {
+            get { return _dependants; }
+            set
+            {
+                _dependants = value;
+            }
+        }
+
+        public IDictionary<IGxEntity, int> Parents
+        {
+            get { return _parents; }
+            set { _parents = value; }
+        }
+
+        public void AddDep(IGxEntity dep)
+        {
+            if (_dependants.ContainsKey(dep))
+                _dependants[dep]++;
+            else
+            {
+                _dependants.Add(dep, 1);
+
+                dep.AddPar(this);
+            }
+        }
+
+        public void AddPar(IGxEntity par)
+        {
+            if (_parents.ContainsKey(par))
+                _parents[par]++;
+            else
+            {
+                _parents.Add(par, 1);
+
+                par.AddDep(this);
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class GxPoint : GxEntityBase, IGxPoint
+    {
+        public int X { get; set; }
+    }
+
+}
---
+++ Src/JsonMagic.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Sdr.JsonMagic
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class JsonMagic
+    {
+        private static StringBuilder _jsonString;
+        private static IDictionary<object, int> _refsDict;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        public static string ToJson(object o)
+        {
+            _jsonString = new StringBuilder();
+            _refsDict = new Dictionary<object, int>();
+
+            _jsonString.Append(ToJsonInternal(o));
+
+            return _jsonString.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        private static string ToJsonInternal(object o, bool isRoot=true, bool isAssigneeSealed=true)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (o != null)
+            {
+                Type type = o.GetType();
+
+                if (!(o is string) && !type.IsPrimitive)
+                {
+                    if (_refsDict.ContainsKey(o))
+                    {
+                        sb.Append(string.Format("{{\"$refid\": {0}}}", _refsDict[o]));
+                        return sb.ToString();
+                    }
+                    else
+                    {
+                        _refsDict.Add(o, _refsDict.Count + 1);
+                    }
+                }
+
+
+                if(type.IsClass || (type.IsGenericType))
+                {
+                    if (o is string)
+                    {
+                        sb.Append("\"");
+                    }
+                    //else if (o is IEnumerable)
+                    //{
+                    //    sb.Append("[");
+                    //}
+                    else
+                    {
+                        if (isRoot || !isAssigneeSealed)
+                        {
+                            sb.Append(string.Format(
+                                "{{\"$type\": \"{0}\", \"$id\": {1},", GetShortTypeName(type), _refsDict[o]));
+                        }
+                    }
+                }
+                else if (type.IsArray)
+                {
+                    sb.Append("[");
+                }
+                else if (type.IsValueType)
+                {
+                    if (type.IsPrimitive)
+                    {
+                    }
+                    else
+                    {
+                        sb.Append("{");
+                    }
+                }
+
+
+                if (type.IsArray)
+                {
+                    foreach (var item in (Array)o)
+                    {
+                        sb.AppendFormat("{0},", ToJsonInternal(item, false));
+                    }
+                }
+                else if(type.IsClass) 
+                {
+                    if (o is string)
+                    {
+                        sb.Append(o);
+                    }
+                    else
+                    {
+                        if (o is IEnumerable)
+                        {
+                            sb.Append("\"$items\": [");
+
+                            // Go through each item in the Enumerable, and Jsonize it...
+                            foreach (var item in (IEnumerable)o)
+                            {
+                                sb.Append(string.Format("{0},", ToJsonInternal(item, false, false)));
+                            }
+
+                            sb.Append("]");
+                        }
+                        else
+                        {
+                            // Get each property and try jsonize them...
+                            var props = type.GetProperties();
+
+                            foreach (var propertyInfo in props)
+                            {
+                                sb.Append(
+                                    string.Format("\"{0}\": {1},", 
+                                        propertyInfo.Name, 
+                                        ToJsonInternal(
+                                            propertyInfo.GetValue(o, null),
+                                            false,
+                                            propertyInfo.PropertyType.IsSealed)));
+                            }
+                        }
+                    }
+                }
+                else if (type.IsValueType)
+                {
+                    if (type.IsPrimitive)
+                    {
+                        sb.Append(o);
+                    }
+                    else
+                    {
+                        var props = type.GetProperties();
+
+                        // Go through each object Property and Jsonize it...
+                        foreach (var propertyInfo in props)
+                        {
+                            sb.Append(
+                                string.Format("\"{0}\": {1},", 
+                                propertyInfo.Name,
+                                ToJsonInternal(propertyInfo.GetValue(o, null))));
+                        }
+                    }
+                }
+
+
+                if(type.IsClass)
+                {
+                    if (o is string)
+                    {
+                        sb.Append("\"");
+                    }
+                    else
+                    {
+                        sb.Append("}");
+                    }
+                }
+                else if (type.IsArray)
+                {
+                    sb.Append("]");
+                }
+                else if (type.IsValueType)
+                {
+                    if (type.IsPrimitive)
+                    {
+                    }
+                    else
+                    {
+                        sb.Append("}");
+                    }
+                }
+
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetShortTypeName(Type type)
+        {
+            if (type != null)
+            {
+                //var str = Regex.Replace(
+                //    type.AssemblyQualifiedName, 
+                //    @"(,\s*Version=(\.?\d*)*)|(,\s*Culture=\w*)|(,\s*PublicKeyToken=\w*)",
+                //    @"");
+
+                return type.AssemblyQualifiedName;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static object FromJson(string json)
+        {
+            // Delegate to the method that actually does the work...
+            return FromJson<object>(json);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static T FromJson<T>(string json)
+            where T:class
+        {
+            if (!string.IsNullOrEmpty(json))
+            {
+                // Check if we are a string...
+                if (json.StartsWith("\"") && json.EndsWith("\""))
+                {
+                    return json.Substring(1, json.Length - 1) as T;
+                }
+                else if (json.StartsWith("{") && json.EndsWith("}"))
+                {
+                    // Split the 'object' string into NAME : VALUE pairs...
+                }
+            }
+
+            return default(T);
+        }
+
+    }
+}
